54a517ee04c3f7dcf6179772e5ff9ef8dba9af84
Christophe PETITJEAN <christophe@famille-petitjean.com>
2024-06-19 15:52:41 +0200

    mobile update

--- src/components/TheMobileMenu.vue
+++ src/components/TheMobileMenu.vue
@@ -1,25 +1,9 @@
 <script setup lang="ts">
-import { computed } from 'vue';
 import TheListLink from '@/components/TheListLink.vue';
 import type { ISbStoriesParams } from '@storyblok/vue';
 import { useStoryblok } from '@storyblok/vue';
-import Banner from '/img/Image_logo_ATC.gif';
 
-const props = defineProps<{
-  modelValue: boolean;
-}>();
-const emits = defineEmits<{
-  'update:modelValue': [value: boolean];
-}>();
-
-const model = computed<boolean>({
-  get: () => {
-    return props.modelValue;
-  },
-  set: (value) => {
-    emits('update:modelValue', value);
-  },
-});
+const model = defineModel<boolean>();
 let option: ISbStoriesParams | undefined = {
   version: 'draft',
 };
@@ -30,14 +14,8 @@ const storyBlokMenu = await useStoryblok('global_menu', option);
 </script>
 
 <template>
-  <v-navigation-drawer v-model="model" fixed class="bg-white forceMaxWidth" width="100%" floating>
-    <template #image>
-      <div class="d-flex h-100 align-center">
-        <v-img :src="Banner" cover></v-img>
-      </div>
-    </template>
-    <!-- <v-app-bar-nav-icon variant="text" @click.stop="emits('update:modelValue', !props.modelValue)"></v-app-bar-nav-icon>
-    <span>AÏKIDO TRADITIONNEL DE LA CÔTIERE</span> -->
+  <v-navigation-drawer v-model="model" class="bg-white" floating>
+    <v-app-bar-nav-icon variant="text" @click="model = false"></v-app-bar-nav-icon>
     <v-list :lines="false">
       <the-list-link v-for="(link, parentIndex) in storyBlokMenu.content.liens" :key="parentIndex" :link="link"> </the-list-link>
     </v-list>
