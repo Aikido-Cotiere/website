82e2ebcc15082a410da02f99953792bdc34c9a1e
ChristPetitjean <christophe@famille-petitjean.com>
2026-05-11 00:23:18 +0200

    Ajout tailwind typo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,8 @@
 import type { Config } from "tailwindcss"
+import tailwindTypography from '@tailwindcss/typography'
+
 export default <Partial<Config>> {
-  content: ["./app/**/*.{vue,js,ts}"]
+  content: ["./app/**/*.{vue,js,ts}"],
+   plugins: [tailwindTypography]
 }
 
