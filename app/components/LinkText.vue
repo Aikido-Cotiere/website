7cca1f75e0c38787b398edaacbbe67a11e174ca3
ChristPetitjean <christophe@famille-petitjean.com>
2026-07-01 10:44:21 +0200

    Fix url dans le body

--- app/components/LinkText.vue
+++ app/components/LinkText.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="hover:underline text-teal-500 cursor-pointer">
+  <PrismicLink class="hover:underline text-teal-500 cursor-pointer" :field="$attrs.node.data">
     <slot />
-  </span>
+  </PrismicLink>
 </template>
